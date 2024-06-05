1b3cd69a6202db652c5815822d7fdbac2f81d38f
lorenzohz <zlorenzoh@gmail.com>
2024-06-04 22:13:27 -0300

    update

---
+++ Listas de Exercícios/Lista 2/ex5.py
@@ -0,0 +1,9 @@
+# Seu programa deve receber o nome do arquivo a ser convertido e o sentido da conversão (se Windows → Linux ou Linux → Windows).
+
+def main() -> None:
+    ...
+
+
+
+if __name__ == '__main__':
+    main()
