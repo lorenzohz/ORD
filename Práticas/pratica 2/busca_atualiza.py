cc374c753ab57c9719eea55ec630dbbdfe58c145
lorenzohz <zlorenzoh@gmail.com>
2024-06-13 11:19:04 -0300

    finished busca_atualiza.py

--- Práticas/pratica 2/busca_atualiza.py
+++ Práticas/pratica 2/busca_atualiza.py
@@ -34,11 +34,53 @@ def main() -> None:
                 arq.write(cabecalho)
 
             elif operacao == '2': #buscar e alterar
-                print('notimplemented')
+                rrn = int(input("Número do registro à ser modificado: "))
+                achou = busca_rrn(arq, rrn)
+                if not achou:
+                    print("Erro, não foi possível encontrar o registro desejado!")
+                else:
+                    confirmacao = input("\nDeseja modificar esse registro (S/N): ")
+                    if confirmacao == "S":
+                        sobrenome = input('Sobrenome: ')
+                        nome = input('Nome: ')
+                        endereco = input('Endereço: ')
+                        cidade = input('Cidade: ')
+                        estado = input('Estado: ')
+                        cep = input('CEP: ')
+                        buffer = (sobrenome + '|' + nome + '|' + endereco + '|' + cidade + '|' + estado + '|' + cep + '|')
+                        buffer_bin = buffer.encode()
+                        buffer_bin = buffer_bin.ljust(64, b'\0')
+                        offset = (rrn * 64) + 4
+                        arq.seek(offset, os.SEEK_SET)
+                        arq.write(buffer_bin)
+
             elif operacao == '3':
-                print('código encerrado!')
+                print('Código encerrado. Arquivo criado/atualizado!')
             else:
                 print("Operação Inválida!")
+        arq.close()
+
+
+def busca_rrn(entrada ,rrn: int) -> bool:
+    '''
+    Recebe o arquivo e o endereço RRN e printa as informações do campo caso exista, enviando uma mensagem de erro caso contrário.
+    Retorna True caso o endereço exista e False caso não.
+    '''
+    entrada.seek(0, os.SEEK_SET)
+    cab = entrada.read(4) # lê cabeçalho
+    num_reg = int.from_bytes(cab)
+    if rrn > num_reg:
+        return False
+    else:
+        offset = (rrn * 64) + 4
+        entrada.seek(offset, os.SEEK_SET)
+        registro = entrada.read(64)
+        registro = registro.decode() #type: ignore
+        registro = registro.split('|') #type: ignore
+        for i in range(len(registro)-1):
+            print(f'Campo {i+1}: {registro[i]}')
+        return True
+
 
 
 if __name__ == '__main__':
